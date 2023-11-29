6e9ac19125e4ac1635e4e396427fa57f8ccd9e79
Rydge <96926831+rydge-rivard@users.noreply.github.com>
2023-11-29 11:14:47 +0100

    Add contact info inputs and state obj for print.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import Contact from "./components/contact.jsx";
+
 function App() {
+  const [person, setPerson] = useState({
+    name: "",
+    address: "",
+    email: "",
+    phone: "",
+  });
+
   return (
     <>
       <h1>CV Builder</h1>
+      <Contact person={person} setPerson={setPerson} />
+      <section className="cv"></section>
+      <div>{person.name}</div>
+      <div>{person.address}</div>
+      <div>{person.email}</div>
+      <div>{person.phone}</div>
     </>
   );
 }
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,3 +1,40 @@
-export default function Contact() {
-  return <h2>Contact</h2>;
+export default function Contact({ person, setPerson }) {
+  return (
+    <section className="contact">
+      <h2>Contact</h2>
+      <Input
+        label="Full Name"
+        typeOf="text"
+        text={person.name}
+        handler={(e) => setPerson({ ...person, name: e.target.value })}
+      ></Input>
+      <Input
+        label="Address"
+        typeOf="text"
+        text={person.address}
+        handler={(e) => setPerson({ ...person, address: e.target.value })}
+      ></Input>
+      <Input
+        label="Email"
+        typeOf="email"
+        text={person.email}
+        handler={(e) => setPerson({ ...person, email: e.target.value })}
+      ></Input>
+      <Input
+        label="Phone Number"
+        typeOf="tel"
+        text={person.phone}
+        handler={(e) => setPerson({ ...person, phone: e.target.value })}
+      ></Input>
+    </section>
+  );
+}
+
+function Input({ label, typeOf, text, handler }) {
+  return (
+    <>
+      <label>{label}</label>
+      <input type={typeOf} value={text} onChange={handler}></input>
+    </>
+  );
 }
